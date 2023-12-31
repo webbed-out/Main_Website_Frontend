d6ed2e38207ace320c1c4705e6eaf6a2f71677cf
Samarth Nehe <samarthnehe2000@gmail.com>
2023-12-31 18:47:12 +0530

    remove aos from about us page

--- src/components/landing-about/LandingAbout.js
+++ src/components/landing-about/LandingAbout.js
@@ -1,32 +1,39 @@
-import React from "react";
-import "./LandingAbout.css";
-import SocialMedia from "../social_media/SocialMedia";
-import Pic1 from "../../images/aboutpic1.png";
-import Pic2 from "../../images/aboutpic2.png";
+import React from 'react';
+import './LandingAbout.css';
+import SocialMedia from '../social_media/SocialMedia';
+import Pic1 from '../../images/aboutpic1.png';
+import Pic2 from '../../images/aboutpic2.png';
 
 function LandingAbout() {
-  return (
-    <div className="landing-about">
-      <div
-        className="landing__about__left"
-        data-aos="fade-right"
-        data-aos-duration="1000"
-      >
-        <h1>
-          Get to<span> know </span>us better
-        </h1>
-        <p>
-          Before starting to work with anyone, you should probably understand
-          them better! So yes, we do it from our side first! Here you go...{" "}
-        </p>
-        <SocialMedia />
-      </div>
-      <div className="about__pics">
-        <img className="about__pic1" src={Pic2} height="253" width="387" />
-        <img className="about__pic2" src={Pic1} height="325" width="510" />
-      </div>
-    </div>
-  );
+    return (
+        <div className='landing-about'>
+            <div className='landing__about__left'>
+                <h1>
+                    Get to<span> know </span>us better
+                </h1>
+                <p>
+                    Before starting to work with anyone, you should probably
+                    understand them better! So yes, we do it from our side
+                    first! Here you go...
+                </p>
+                <SocialMedia />
+            </div>
+            <div className='about__pics'>
+                <img
+                    className='about__pic1'
+                    src={Pic2}
+                    height='253'
+                    width='387'
+                />
+                <img
+                    className='about__pic2'
+                    src={Pic1}
+                    height='325'
+                    width='510'
+                />
+            </div>
+        </div>
+    );
 }
 
 export default LandingAbout;
--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,52 +1,53 @@
-import React from "react";
-import "./Work.css";
-import SocialMedia from "../social_media/SocialMedia";
-import WorkPic1 from "../../images/WorkPic1.png";
-import WorkPic2 from "../../images/WorkPic2.png";
+import React from 'react';
+import './Work.css';
+import SocialMedia from '../social_media/SocialMedia';
+import WorkPic1 from '../../images/WorkPic1.png';
+import WorkPic2 from '../../images/WorkPic2.png';
 
 function Work() {
-  return (
-    <div className="work">
-      <div className="work__container">
-        <div
-          className="work__left"
-          data-aos="fade-right"
-          data-aos-duration="1000"
-        >
-          <h1>
-            Come check our previous <span>works</span>
-          </h1>
-          <p>
-            Before starting to work with anyone, you should probably understand
-            them better! So yes, we do it from our side first! Here you go...{" "}
-          </p>
-          <SocialMedia />
+    return (
+        <div className='work'>
+            <div className='work__container'>
+                <div
+                    className='work__left'
+                    data-aos='fade-right'
+                    data-aos-duration='1000'
+                >
+                    <h1>
+                        Come check our previous <span>works</span>
+                    </h1>
+                    <p>
+                        Before starting to work with anyone, you should probably
+                        understand them better! So yes, we do it from our side
+                        first! Here you go...
+                    </p>
+                    <SocialMedia />
+                </div>
+                <div className='work__right'>
+                    <div className='work__images'>
+                        <img
+                            alt='image2'
+                            data-aos='fade-right'
+                            data-aos-duration='1000'
+                            className='work__pic2'
+                            src={WorkPic2}
+                            height='385'
+                            width='620'
+                        />
+                        <img
+                            alt='image2'
+                            data-aos='fade-left'
+                            data-aos-duration='1000'
+                            className='work__pic1'
+                            src={WorkPic1}
+                            height='280'
+                            width='481'
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="work__right">
-          <div className="work__images">
-            <img
-              alt="image2"
-              data-aos="fade-right"
-              data-aos-duration="1000"
-              className="work__pic2"
-              src={WorkPic2}
-              height="385"
-              width="620"
-            />
-            <img
-              alt="image2"
-              data-aos="fade-left"
-              data-aos-duration="1000"
-              className="work__pic1"
-              src={WorkPic1}
-              height="280"
-              width="481"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Work;
