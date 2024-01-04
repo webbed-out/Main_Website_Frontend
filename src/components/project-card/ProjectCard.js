ead61e579630b84c74d097d578b52967c0c22cff
mmt10111 <samarth.nehe@go-mmt.com>
2024-01-04 12:19:18 +0530

    add tabs in works

--- src/components/project-card/ProjectCard.js
+++ src/components/project-card/ProjectCard.js
@@ -1,19 +1,16 @@
-import React from "react";
-import "./ProjectCard.css";
-import website from "../../images/website.png";
+import React from 'react';
+import './ProjectCard.css';
+import website from '../../images/website.png';
 
-function ProjectCard({ image, name, content, reverse, link }) {
+function ProjectCard({ image, name, content, reverse, link, type }) {
   return (
     <>
       {reverse === 1 ? (
-        <div
-          className="project-card"
-          data-aos="fade-left"
-          data-aos-duration="1000"
-        >
+        <div className="project-card" data-aos="fade-left" data-aos-duration="1000">
           <div className="project-card-right">
             <div className="project-card-right-content">
-              <h1 style={{ textAlign: "right" }}>{name}</h1>
+              <h5 style={{ textAlign: 'right', fontSize: 18, color: '#00563B' }}>{type}</h5>
+              <h1 style={{ textAlign: 'right' }}>{name}</h1>
               <p>{content}</p>
             </div>
 
@@ -28,16 +25,13 @@ function ProjectCard({ image, name, content, reverse, link }) {
           </div>
         </div>
       ) : (
-        <div
-          className="project-card"
-          data-aos="fade-right"
-          data-aos-duration="1000"
-        >
+        <div className="project-card" data-aos="fade-right" data-aos-duration="1000">
           <div className="project-card-left">
             <img src={image} />
           </div>
           <div className="project-card-right">
             <div className="project-card-right-content">
+              <h5 style={{ fontSize: 18, color: '#00563B' }}>{type}</h5>
               <h1>{name}</h1>
               <p>{content}</p>
             </div>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,39 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Projects.css';
 import ProjectCard from '../project-card/ProjectCard';
 // import Project1 from '../../images/Project1.png';
 // import Amazon from '../../images/amazon.png';
 import axios from 'axios';
 import { apiPaths, WEBBEDOUT_API_ENDPOINT } from '../../configs/webbedOutConfig';
-
-function Button(props) {
-  return (
-    <div
-      className="button"
-      style={{
-        backgroundColor: props.isActive ? 'rbga(0,148,255,0.5)' : '#FFFFFF',
-        border: props.isActive ? '2px solid #00A6FB' : '2px solid #FFFFFF',
-      }}
-    >
-      <p
-        className="button-text"
-        style={{
-          color: props.isActive ? '#00A6FB' : '#4A4A4A',
-          fontWeight: props.isActive ? '700' : '400',
-        }}
-      >
-        {props.label}
-      </p>
-    </div>
-  );
-}
+import Loader from '../loader/Loader';
 
 function Tabs(props) {
   const tabsList = ['ALL', 'DESKTOP', 'WEB', 'APP'];
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const handleTabPress = (index) => {
+  const handleTabPress = (index, type) => {
     setActiveIndex(index);
+    props.changeSelectedType(type);
   };
 
   return (
@@ -41,7 +21,7 @@ function Tabs(props) {
       {tabsList.map((item, index) => {
         return (
           <Button
-            onPress={() => handleTabPress(index)}
+            onPress={() => handleTabPress(index, item)}
             label={item}
             isActive={index === activeIndex}
           />
@@ -55,8 +35,9 @@ function Button(props) {
   return (
     <div
       className="button"
+      onClick={props.onPress}
       style={{
-        backgroundColor: props.isActive ? 'rbga(0,148,255,0.5)' : '#FFFFFF',
+        backgroundColor: props.isActive ? '#00A6FB50' : '#FFFFFF',
         border: props.isActive ? '2px solid #00A6FB' : '2px solid #FFFFFF',
       }}
     >
@@ -69,43 +50,48 @@ function Button(props) {
       >
         {props.label}
       </p>
-    </div>
-  );
-}
-
-function Tabs(props) {
-  const tabsList = ['ALL', 'DESKTOP', 'WEB', 'APP'];
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const handleTabPress = (index) => {
-    setActiveIndex(index);
-  };
-
-  return (
-    <div className="tabsContainer">
-      {tabsList.map((item, index) => {
-        return (
-          <Button
-            onPress={() => handleTabPress(index)}
-            label={item}
-            isActive={index === activeIndex}
-          />
-        );
-      })}
     </div>
   );
 }
 
 function Projects() {
-  const [works, setWorks] = useState([]);
+  const [works, setWorks] = useState(null);
+  const [selectedType, setSelectedType] = useState('ALL');
+  const workGrouped = {
+    ['WEB']: [],
+    ['DESKTOP']: [],
+    ['APP']: [],
+    ['ALL']: [],
+  };
+
+  const changeSelectedType = (type) => {
+    setSelectedType(type);
+  };
+
+  const pairProjectsWithType = (data) => {
+    data.forEach((item, index) => {
+      workGrouped['ALL'].push(item);
+      if (item?.type) {
+        workGrouped[item.type].push(item);
+      }
+    });
+    return workGrouped;
+  };
 
   useEffect(() => {
     async function getWorksDetails() {
-      const response = await axios.get(`${WEBBEDOUT_API_ENDPOINT}${apiPaths.work}`);
-      setWorks(response.data);
+      axios
+        .get(`${WEBBEDOUT_API_ENDPOINT}${apiPaths.work}`)
+        .then((response) => {
+          setWorks(pairProjectsWithType(response.data));
+        })
+        .catch((e) => {
+          console.log('[WEBBEDOUT][ERROR] - ', e);
+        });
     }
     getWorksDetails();
   }, []);
+
   return (
     <div id="work" className="project">
       <div className="project__container">
@@ -114,20 +100,33 @@ function Projects() {
           <h1>What have we created?</h1>
         </div>
         <div className="tabs">
-          <Tabs />
+          <Tabs changeSelectedType={changeSelectedType} />
         </div>
-
-        {works.map((work, index) => {
-          return (
-            <ProjectCard
-              name={work.title}
-              content={work.description}
-              image={work.photo}
-              reverse={index % 2}
-              link={work.url}
-            />
-          );
-        })}
+        {works && Object.keys(works) && works?.[selectedType].length ? (
+          works?.[selectedType]?.map((work, index) => {
+            return (
+              <ProjectCard
+                name={work.title}
+                content={work.description}
+                image={work.photo}
+                reverse={index % 2}
+                link={work.url}
+                type={work?.type}
+              />
+            );
+          })
+        ) : (
+          <div
+            style={{
+              height: '40vh',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <h3 style={{ fontSize: '24px' }}>No projects of this type :(</h3>
+          </div>
+        )}
       </div>
     </div>
   );
